ea4e84c0e669232eadbb3d184bde9c787feaabfd
Frykits <88166479+Frykits@users.noreply.github.com>
2024-05-06 18:06:41 -0300

    teste

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	M := 10
-	N := 10
+	M, N := 10, 10
 
-	grid := [][]int{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	// Initializing the grid.
+	grid := [][]int{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -20,6 +19,7 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	}
 
+	// Displaying the grid
 	fmt.Println("Original Generation")
 	for i := 0; i < M; i++ {
 		for j := 0; j < N; j++ {
@@ -35,35 +35,45 @@ func main() {
 	nextGeneration(grid, M, N)
 }
 
+// Function to print next generation
 func nextGeneration(grid [][]int, M int, N int) {
-	future := make([][]int, M, N)
+	future := make([][]int, M)
+	for i := range future {
+		future[i] = make([]int, N)
+	}
+
+	// Loop through every cell
 	for l := 0; l < M; l++ {
 		for m := 0; m < N; m++ {
-			aliveNeighbors := 0
+			// finding no Of Neighbours that are alive
+			aliveNeighbours := 0
 			for i := -1; i <= 1; i++ {
 				for j := -1; j <= 1; j++ {
-					if i == 0 && j == 0 {
-						continue
-					}
-					if l+i >= 0 && l+i < M && m+j >= 0 && m+j < N && grid[l+i][m+j] == 1 {
-						aliveNeighbors++
+					if l+i >= 0 && l+i < M && m+j >= 0 && m+j < N {
+						aliveNeighbours += grid[l+i][m+j]
 					}
 				}
 			}
-			aliveNeighbors = aliveNeighbors - grid[l][m]
 
-			if grid[l][m] == 1 && aliveNeighbors < 2 {
+			// The cell needs to be subtracted from
+			// its neighbours as it was counted before
+			aliveNeighbours -= grid[l][m]
+
+			// Implementing the Rules of Life
+
+			// Cell is lonely and dies
+			if grid[l][m] == 1 && aliveNeighbours < 2 {
 				future[l][m] = 0
-			} else if grid[l][m] == 1 && aliveNeighbors > 3 {
+			} else if grid[l][m] == 1 && aliveNeighbours > 3 { // Cell dies due to over population
 				future[l][m] = 0
-			} else if grid[l][m] == 0 && aliveNeighbors == 3 {
+			} else if grid[l][m] == 0 && aliveNeighbours == 3 { // A new cell is born
 				future[l][m] = 1
-			} else {
+			} else { // Remains the same
 				future[l][m] = grid[l][m]
 			}
-
 		}
 	}
+
 	fmt.Println("Next Generation")
 	for i := 0; i < M; i++ {
 		for j := 0; j < N; j++ {
